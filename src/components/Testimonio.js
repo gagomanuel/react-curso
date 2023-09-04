ff4b7695bfe4b4c642a2710bbf8518d5c42513fa
Gago Manuel <manuel.gago@arba.gov.ar>
2023-09-04 15:33:20 -0300

    texto con css Min 2.21.02

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import '../styles/Testimonio.css'
 
 export function Testimonio() {
   return (
-    <div className='contenedor-testimonio'>
+    <div className='contenedorTestimonio'>
       <img 
-      className= 'img-testimonio' 
+      className= 'imgTestimonio' 
       src= {require ('../imgs/fcc-emma.png')}
       alt= 'Foto de Emma'/>
-      <div className= 'contenedor-texto-testimonio'>
-        <h3 className= 'nombre-testimonio'>Emma Bostian in Sweden</h3>
-        <h3 className= 'cargo-testimonio'>Software Engineer at Spotify</h3>
-        <p className= 'testo-testimonio'>I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+      <div className= 'contenedorTextoTestimonio'>
+        <h3 className= 'nombreTestimonio'>Emma Bostian in Sweden</h3>
+        <h3 className= 'cargoTestimonio'>Software Engineer at Spotify</h3>
+        <p className= 'textoTestimonio'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
       </div>
     </div>
   );

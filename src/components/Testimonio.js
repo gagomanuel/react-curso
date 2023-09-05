d1c3f6a480ea2fcec51bc29200bfc3f846a264a8
gagomanuel <manuelmeg96@gmail.com>
2023-09-04 21:29:35 -0300

    testimonios x3 OK min 2.37.20

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import '../styles/Testimonio.css'
 
-export function Testimonio() {
+export function Testimonio(props) {
   return (
     <div className='contenedorTestimonio'>
       <img 
       className= 'imgTestimonio' 
-      src= {require ('../imgs/fcc-emma.png')}
+      src= {require (`../imgs/fcc-${props.imagen}.png`)}
       alt= 'Foto de Emma'/>
       <div className= 'contenedorTextoTestimonio'>
-        <h3 className= 'nombreTestimonio'>Emma Bostian in Sweden</h3>
-        <h3 className= 'cargoTestimonio'>Software Engineer at Spotify</h3>
-        <p className= 'textoTestimonio'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+        <h3 className= 'nombreTestimonio'>{props.nombre} in {props.pais}</h3>
+        <h3 className= 'cargoTestimonio'>{props.cargo} at {props.empresa}</h3>
+        <p className= 'textoTestimonio'>"{props.testimonio}"</p>
       </div>
     </div>
   );
